e8b4b744e091882fd18be2447acc4a0f3c6a8c08
DocDomergue <97396135+DocDomergue@users.noreply.github.com>
2023-10-05 12:40:48 -0400

    Pls work

---
+++ UVM-StudyPal/UVM_StudyPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  UVM_StudyPalApp.swift
+//  UVM-StudyPal
+//
+//  Created by Kevin Encarnacao on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct UVM_StudyPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
